ab981affae469d3fab1409c2285789d0464a83b0
charkops <charis.kopsacheilis@gmail.com>
2020-05-27 12:34:36 +0300

    Implement install script

--- install.py
+++ install.py
@@ -2,18 +2,29 @@
 
 import sys
 import os
-
+from pathlib import Path
+import shutil
 
 
 
 
 def main(args):
   # Check if ~/bin exists
-  if os.path.isdir('~/bin'):    
-    print('~/bin exists')
-  else:
-    print('no')
+  home = str(Path.home())
+  bin = home + '/bin'  
+  if not os.path.isdir(bin):
+    os.mkdir(bin)
+  
+  ## Copy createCmakeProject to bin folder
+  src = 'createCmakeProject.py' 
+  dst = bin + '/createCmakeProject'
+  try:
+    shutil.copy(src, dst)
+  except IOError:
+    print ('Could not copy script to /bin')
+    sys.exit(-1)
 
+  print('Done, script added to ~/bin')
 
 if __name__ == '__main__':
   main(sys.argv)
